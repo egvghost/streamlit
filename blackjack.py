6497a6e5110437cca4651b3764e8eb46acd3c16c
egvghost <egvghost@gmail.com>
2025-01-31 19:55:06 -0300

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -174,6 +174,8 @@ if not st.session_state.game_over:
         if st.button("Stand", key="stand"):
             st.session_state.hide_card = False
             dealer_points = get_points(st.session_state.dealer_cards, st.session_state.hide_card)
+            check_game_status()
+            st.rerun()
             while dealer_points < 17:
                 st.session_state.dealer_cards.append(hit())
                 check_game_status()
