5fdccd8fb2124c42cfce30b242562db021d80388
egvghost <egvghost@gmail.com>
2025-01-31 16:25:15 -0300

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -173,6 +173,8 @@ if not st.session_state.game_over:
             st.session_state.dealer_cards.append(hit())
             dealer_points = get_points(st.session_state.dealer_cards, st.session_state.hide_card)
         check_game_status()
+        st.write(f"Dealer: {dealer_points} {st.session_state.dealer_bj}")
+        show_cards(st.session_state.dealer_cards, st.session_state.hide_card)
 
 st.write(st.session_state.message)
 
