55d93b1c6159cfcb364d0d68c532f91b499f0d3c
egvghost <egvghost@gmail.com>
2025-01-31 17:35:03 -0300

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -163,7 +163,7 @@ show_cards(st.session_state.player_cards)
 
 # Adding unique keys to buttons to force rerun
 if not st.session_state.game_over:
-    col1, col2 = st.columns([1, 1], gap="small")
+    col1, col2, col3 = st.columns([1, 1, 12], gap="small")
     with col1:
         if st.button("Hit", key="hit"):
             st.session_state.player_cards.append(hit())
@@ -173,11 +173,13 @@ if not st.session_state.game_over:
         if st.button("Stand", key="stand"):
             st.session_state.hide_card = False
             dealer_points = get_points(st.session_state.dealer_cards, st.session_state.hide_card)
-            while dealer_points < 17 or (dealer_points < player_points and dealer_points < 21):
+            while (dealer_points < 17) and (dealer_points < player_points):
                 st.session_state.dealer_cards.append(hit())
                 dealer_points = get_points(st.session_state.dealer_cards, st.session_state.hide_card)
                 check_game_status()
                 st.rerun()
+    with col3:
+        pass
 
 st.write(st.session_state.message)
 
