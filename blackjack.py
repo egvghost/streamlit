ffa0ce8fb0b37d52d17faa5b06646110dbf5e95c
egvghost <egvghost@gmail.com>
2025-01-31 18:34:41 -0300

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -182,14 +182,8 @@ if not st.session_state.game_over:
     with col3:
         pass
 
-col1, col2, col3 = st.columns([1, 1, 7], gap="small")
-with col1:
-    if st.session_state.game_over and st.button("Play again", key="play_again"):
-        reset_game()
-        st.rerun()
-with col2:
-    st.write(st.session_state.message)
-with col3:
-    pass
+st.write(st.session_state.message)
 
-
+if st.session_state.game_over and st.button("Play again", key="play_again"):
+    reset_game()
+    st.rerun()
