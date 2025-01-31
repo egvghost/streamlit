7f9a22472fa3ffd0b932b57dcc23deb79cc128aa
egvghost <egvghost@gmail.com>
2025-01-31 17:08:11 -0300

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -166,6 +166,7 @@ if not st.session_state.game_over:
     if st.button("Hit", key="hit"):
         st.session_state.player_cards.append(hit())
         check_game_status()
+        st.rerun()
     if st.button("Stand", key="stand"):
         st.session_state.hide_card = False
         dealer_points = get_points(st.session_state.dealer_cards, st.session_state.hide_card)
@@ -173,6 +174,7 @@ if not st.session_state.game_over:
             st.session_state.dealer_cards.append(hit())
             dealer_points = get_points(st.session_state.dealer_cards, st.session_state.hide_card)
             check_game_status()
+            st.rerun()
 
 st.write(st.session_state.message)
 
