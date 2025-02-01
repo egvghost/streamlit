507b93663a8342c3849bfed15e408976f26e3464
egvghost <egvghost@gmail.com>
2025-01-31 21:04:17 -0300

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -173,17 +173,19 @@ if not st.session_state.game_over:
             st.rerun()
     with col2:
         if st.button("Stand", key="stand") or dealer_turn:
-            dealer_turn = False
             st.session_state.hide_card = False
             dealer_points = get_points(st.session_state.dealer_cards, st.session_state.hide_card)
             check_game_status()
+            if not dealer_turn:
+                dealer_turn = True
+                st.rerun()
             while dealer_points < 17:
                 st.session_state.dealer_cards.append(hit())
                 dealer_points = get_points(st.session_state.dealer_cards, st.session_state.hide_card)
                 check_game_status()
                 time.sleep(1)
-                dealer_turn = True
                 st.rerun()
+            dealer_turn = False
             st.rerun()
     with col3:
         pass
